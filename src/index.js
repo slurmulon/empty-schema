0c04b37d4fa0b94274c63f5eab56df1b61359650
Erik Vavro <erik@sighten.io>
2016-06-16 00:46:26 -0700

    :package: updated packaging so that module doesnt need to be compiled postinstall

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import deref  from 'simple-json-schema-deref'
 import _empty from './empty'
 
 export { _empty }   from './empty'
-export { _array }   from './array'
-export { _boolean } from './boolean'
-export { _integer } from './integer'
-export { _number }  from './number'
-export { _null }    from './null'
-export { _object }  from './object'
-export { _string }  from './string'
-export { merge }    from './merge'
+export { _array   as array   } from './array'
+export { _boolean            } from './boolean'
+export { _integer as integer } from './integer'
+export { _number  as number  } from './number'
+export { _null               } from './null'
+export { _object  as object  } from './object'
+export { _string  as string  }  from './string'
+export { merge    as merge   }  from './merge'
 
 export const empty = (schema) => {
   const denormed = deref(schema)
